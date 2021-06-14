5df433ffa28d404b32090aee7cece30e56e8476b
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 20:35:18 +0700

    Câu lệnh rẽ nhánh

---
+++ CSC.cpp
@@ -0,0 +1,17 @@
+﻿/*
+Cho ba số nguyên m, n, k (m < n < k). Nếu ba số này lập thành một cấp số cộng thì thì tính tổng 3 số này còn không thông báo không phải là cấp số cộng.
+
+ Em hãy lập trình thực hiện yêu cầu trên. Với m, n, k là các số nguyên nhập vào từ bàn phím.
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	int m, n, k;
+	cin >> m >> n >> k;
+	if ((n - m) == (k - n)) {
+		cout << m + n + k;
+	}
+	else {
+		cout << "KHONG PHAI CAP SO CONG";
+	}
+}
