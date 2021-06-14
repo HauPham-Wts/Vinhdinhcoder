2bb8b7e2b37afa014b3b51f899a55c5ef41e5a5e
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 18:34:49 +0700

    Update QMOBILE.cpp

--- QMOBILE.cpp
+++ QMOBILE.cpp
@@ -1,4 +1,8 @@
-﻿#include<iostream>
+/*
+Bạn Lan sử dụng điện thoại Q.Mobile, bạn thắc mắc là làm thế nào mà khi có người gọi điện thoại đến thì điện thoại có thể đọc số của người gọi đến. Ví dụ 0942250787 là 0   9   4   2   2   5   0   7   8   7.
+Em hãy lập trình giúp cho bạn nhập vào một số điện thoại có 10 chữ số và đọc các số này ra màn hình, các số cách nhau một dấu cách.
+*/
+#include<iostream>
 #include<stack>
 using namespace std;
 int main() {
