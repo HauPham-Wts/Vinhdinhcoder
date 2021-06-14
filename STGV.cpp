082cc6db8da142d26020d2364fe1e00f6a6d2222
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 16:10:54 +0700

    Add files via upload

---
+++ STGV.cpp
@@ -0,0 +1,9 @@
+﻿/*Lập trình tính và thông báo ra màn hình diện tích tam giác vuông với hai cạnh góc vuông a và b là các số nguyên (1 ≤ a, b ≤ 106). Kết quả làm tròn 2 chữ số thập phân.*/
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+	double a, b;
+	cin >> a >> b;
+	cout <<setprecision(2)<<fixed<< (a * b) / 2;
+}
