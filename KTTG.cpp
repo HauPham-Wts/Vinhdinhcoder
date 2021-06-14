a1ceace8cc156a145eea87403d7e003d1ea56afd
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 20:21:54 +0700

    Câu lệnh rẽ nhánh

---
+++ KTTG.cpp
@@ -0,0 +1,16 @@
+﻿/*
+Lập trình nhập vào từ bàn phím 3 số nguyên x, y, z là ba cạnh của một tam giác. 
+Kiểm tra và thông báo ra màn hình đây có phải là 3 cạnh của một tam giác cân hay không.
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	long long x, y, z;
+	cin >> x >> y >> z;
+	if (x == y || x == z || y == z) {
+		cout << "LA TAM GIAC CAN";
+	}
+	else {
+		cout << "KHONG PHAI TAM GIAC CAN";
+	}
+}
