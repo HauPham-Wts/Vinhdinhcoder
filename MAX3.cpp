ba471f280eb03d4d565d37941a01e3b076a0c6ee
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 20:16:00 +0700

    Câu lệnh rẽ nhánh

---
+++ MAX3.cpp
@@ -0,0 +1,26 @@
+﻿/*
+Lập trình tìm số lớn nhất (Max) trong 3 số nguyên a, b, c được nhập vào từ bàn phím.
+
+Dữ liệu nhập:
+
+  - Gồm 3 số nguyên a, b, c theo thứ tự, cách nhau một dấu cách.
+
+Kết quả:
+
+  - in đáp án chính xác theo yêu cầu
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	long long a, b, c;
+	cin >> a >> b >> c;
+	if (a >= b && a >= c) {
+		cout << "So lon nhat: " << a;
+	}
+	else if (b >= c) {
+		cout << "So lon nhat: " << b;
+	}
+	else {
+		cout << "So lon nhat: " << c;
+	}
+}
