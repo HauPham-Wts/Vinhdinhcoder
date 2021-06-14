72cbba1792bea3a968ac6cca3d4066791608f2c8
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 20:41:03 +0700

    Câu lệnh rẽ nhánh

---
+++ CSN.cpp
@@ -0,0 +1,17 @@
+﻿/*
+Cho ba số nguyên p, q, r (p < q < r và ≠ 0). Kiểm tra ba số này theo thứ tự nhập vào có tạo thành một cấp số nhân hay không.
+
+Em hãy lập trình thực hiện yêu cầu trên. Kết quả thông báo ra YES hoặc NO tương ứng.
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	double p, q, r;
+	cin >> p >> q >> r;
+	if ((q / p) == (r / q)) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+}
