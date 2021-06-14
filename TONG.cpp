4ad055bc209e8326fae59ca8cfe45a74fe27748d
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 16:12:13 +0700

    Tính toán đơn giản

---
+++ TONG.cpp
@@ -0,0 +1,18 @@
+﻿/*
+Cho hai số nguyên dương A và B, hãy tính tổng A+B.
+
+Dữ liệu nhập:
+
+- Là hai số nguyên A, B ( 0 < A, B < 2.109), A và B cách nhau một khoảng trắng.
+
+Dữ liệu xuất:
+
+- Là tổng của A và B
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	long long a, b;
+	cin >> a >> b;
+	cout << a + b;
+}
