6a2d32a93c4670bd8e5d163018c6b78493b34a4b
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 17:09:24 +0700

    Tính toán đơn giản

---
+++ QMOBILE.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<stack>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	stack<int> s;
+	while (n != 0) {
+		int temp = n % 10;
+		s.push(temp);
+		n /= 10;
+	}
+	cout << 0 << " ";
+	while (!s.empty()) {
+		cout << s.top() << " ";
+		s.pop();
+	}
+}
