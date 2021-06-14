b607bd3085e37bf6342ef8022977bb9d1ea62a4f
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 16:04:46 +0700

    Tính toán đơn giản

---
+++ BIEUTHUC.cpp
@@ -0,0 +1,8 @@
+﻿/*Lập trình nhập từ bàn phím các số nguyên a, b, c, d và x từ bàn phím. Tính và đưa ra màn hình giá trị biểu thức ax3 + bx2 + cx + d? (1 ≤ a, b, c, d, x ≤ 104)*/
+#include<iostream>
+using namespace std;
+int main() {
+	long long a, b, c, d, x;
+	cin >> a >> b >> c >> d >> x;
+	cout << a * (x * x * x) + b * (x * x) + c * x + d;
+}
