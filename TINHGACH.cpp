2b231280a3657518f62a3abb7a889b2d5c03315f
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 16:11:57 +0700

    Tính toán đơn giản

---
+++ TINHGACH.cpp
@@ -0,0 +1,14 @@
+﻿/*Mỗi hộ dân ở khu làng sinh thái Đông Triệu Phong được cung cấp vật liệu để xây dựng một căn nhà cấp 4 có sàn là HCN kích thước (x, y) đơn vị mét. Tường xây cao z mét, nóc cao t mét (tính từ sàn). Ban quản lý muốn biết cần cung cấp cho mỗi hộ bao nhiêu viên gạch để xây 4 bức tường. Biết rằng theo định mức xây dựng số gạch được tính 39 viên / m2. Giả sử rằng người ta không tính đến diện tích phần làm cửa.
+Bạn hãy lập trình giúp cho bản quản lý dự án tính số lượng viên gạch ít nhất cần cung cấp cho các hộ dân. (Với x, y, z, t là các số nguyên dương < 20, đơn vị tính là mét).
+
+Đáp án in ra màn hình là một số nguyên S (là số lượng viên gạch cần cung cấp cho các hộ dân). Đáp án của bạn được chấm đúng nếu sai lệch không quá 1 viên so với đáp án của ban tổ chức.
+
+Dữ liệu nhập: x, y, z, t lần lượt được nhập từ bàn phím
+
+Đáp án: in ra một số nguyên duy nhất là số lượng viên gạch.
+*/
+#include<iostream>
+using namespace std;
+int main() {
+
+}
