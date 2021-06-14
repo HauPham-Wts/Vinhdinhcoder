9d09ee1d0477d7b43706991019a44003cbdbdd9a
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 21:48:58 +0700

    Câu lệnh rẽ nhánh

---
+++ TRANSL.cpp
@@ -0,0 +1,35 @@
+﻿/*
+Chương trình dịch tiếng Anh các thứ trong tuần.
+
+Em hãy lập trình cho phép người dùng nhập vào một số nguyên từ 2 đến 8 và thông báo thứ tương ứng ra màn hình.
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	switch (n) {
+	case 2:
+		cout << "Monday";
+		break;
+	case 3:
+		cout << "Tuesday";
+		break;
+	case 4:
+		cout << "Wednesday";
+		break;
+	case 5:
+		cout << "Thursday";
+		break;
+	case 6:
+		cout << "Friday";
+		break;
+	case 7:
+		cout << "Saturday";
+		break;
+	case 8:
+		cout << "Sunday";
+		break;
+	}
+	return 0;
+}
