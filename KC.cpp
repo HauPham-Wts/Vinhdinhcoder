52840d800d0a7295aac0bad70921bfc60738ee03
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 16:09:40 +0700

    Tính toán đơn giản

---
+++ KC.cpp
@@ -0,0 +1,10 @@
+﻿/*Một người đi xe đạp với tốc độ 10km/h và một người đi xe máy với tốc độ 30km/h cùng xuất phát tại một vị trí, cùng thời điểm và cùng hướng.
+
+ Lập trình tính khoảng cách giữa hai người sau t giờ (t là số nguyên dương, t≤15). Dữ liệu t được nhập vào từ bàn phím, kết quả in ra màn hình.*/
+#include<iostream>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;
+	cout << (30 * t) - (10 * t);
+}
