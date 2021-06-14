f2a1b825c4fd48ee55560a16782fe5349a0b61f6
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 16:10:11 +0700

    Tính toán đơn giản

---
+++ SHCN.cpp
@@ -0,0 +1,10 @@
+﻿/*
+ Lập trình tính và thông báo ra màn hình diện tích hình chữ nhật với hai cạnh a và b là các số nguyên được nhập vào từ bàn phím (1 ≤ a,b ≤ 106)?
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	long long a, b;
+	cin >> a >> b;
+	cout << a * b;
+}
