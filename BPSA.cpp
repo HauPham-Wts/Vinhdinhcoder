73ea7bd1ecef229625eb1477e4ef360018ea3bf0
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 20:32:01 +0700

    Câu lệnh rẽ nhánh

---
+++ BPSA.cpp
@@ -0,0 +1,40 @@
+﻿/*
+Lập trình nhập vào từ bàn phím ba số nguyên m, n, k. Tính và thông báo ra màn hình bình phương của số âm và lập phương của số dương.
+
+Dữ liệu vào:
+
+  - m, n, k nhập vào từ bàn phím
+
+Kết quả:
+
+  - Thông báo ra màn hình kết quả tương ứng
+*/
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+int main() {
+	int m, n, k;
+	cin >> m >> n >> k;
+	if (m > 0) {
+		cout << setprecision(0) << fixed << pow(m, 3);
+	}
+	else {
+		cout << setprecision(0) << fixed << pow(m, 2);
+	}
+	cout << " ";
+	if (n > 0) {
+		cout << setprecision(0) << fixed << pow(n, 3);
+	}
+	else {
+		cout << setprecision(0) << fixed << pow(n, 2);
+	}
+	cout << " ";
+	if (k > 0) {
+		cout << setprecision(0) << fixed << pow(k, 3);
+	}
+	else {
+		cout << setprecision(0) << fixed << pow(k, 2);
+	}
+	return 0;
+}
