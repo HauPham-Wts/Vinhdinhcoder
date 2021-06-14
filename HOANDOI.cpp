a7c7e937c76fb3f535e11e5c3d986d64fbaa796a
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-14 16:09:20 +0700

    Tính toán đơn giản

---
+++ HOANDOI.cpp
@@ -0,0 +1,11 @@
+﻿/*Lập trình nhập vào từ bàn phím 2 số nguyên a và b, hoán đổi giá trị của chúng cho nhau rồi in kết quả 2 số đã hoán đổi ra màn hình.*/
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int c = a;
+	a = b;
+	b = c;
+	cout << a << " " << b;
+}
