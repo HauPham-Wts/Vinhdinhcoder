62c3b6fff1046520e1f11df6f29146f8ba76d523
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-15 16:53:52 +0700

    Vòng lặp FOR

---
+++ TONGLE.cpp
@@ -0,0 +1,18 @@
+﻿/*
+Lập trình tính tổng các số lẻ từ 1  đến N (1 < N < 1 triệu). Thông báo kết quả ra màn hình.
+
+T = 1 + 3 + ... + ...
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	unsigned long long n, sum = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 != 0) {
+			sum += i;
+		}
+	}
+	cout << sum;
+	return 0;
+}
