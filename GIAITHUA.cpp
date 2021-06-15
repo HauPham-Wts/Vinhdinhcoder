612c34c5514f61b078183a870c3eeb64eb7dd266
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-15 16:57:59 +0700

    Vòng lặp FOR

---
+++ GIAITHUA.cpp
@@ -0,0 +1,16 @@
+﻿/*
+Lập trình nhập n từ bàn phím (0 < n < 20), tính giai thừa của n (n!).
+
+N! = 1 * 2 * 3 * ... * N
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	unsigned long long n, mul = 1;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		mul *= i;
+	}
+	cout << mul;
+	return 0;
+}
