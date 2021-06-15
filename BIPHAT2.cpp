7ff91f7e96cff7ddea61f9faee4bae687bbf7e63
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-15 17:10:56 +0700

    Vòng lặp FOR

---
+++ BIPHAT2.cpp
@@ -0,0 +1,28 @@
+﻿/*
+ Thầy Phú phát kẹo cho các em học sinh theo qui tắc sau: Em đầu tiên được nhận 1 viên kẹo, em tiếp theo nhận nhiều hơn em trước đó 3 viên. 
+ Lớp có N học sinh tuy nhiên Thầy chỉ mang theo K cái kẹo thôi. Bạn hãy tính giúp thầy số lượng học sinh nhiều nhất nhận được kẹo. 
+ Thầy Phú rất nguyên tắc nên phát đúng theo quy định trên, kẹo còn thừa không đủ để phát theo quy tắc trên thì Thầy đem về cho cu Sắn.
+Dữ liệu nhập:
+- Gồm hai số nguyên dương N, K
+Kết quả xuất:
+- in ra một số nguyên duy nhất là số lượng bạn nhận được kẹo
+Ràng buộc:
+- 1 < N < 105
+- 1 < K < 1015
+ */
+#include<iostream>
+using namespace std;
+int main() {
+	unsigned long long n, k, sum = 0, temp = 1, hmm = 0;
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		sum += temp;
+		if (sum <= k) {
+			hmm++;
+		}
+		temp += 3;
+	}
+	cout << hmm;
+	return 0;
+}
+
