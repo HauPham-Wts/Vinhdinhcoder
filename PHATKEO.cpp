561b45e2eb5ef621ab28bf0e5a8c61900ba58f52
HauPham-Wts <76433459+HauPham-Wts@users.noreply.github.com>
2021-06-15 16:59:10 +0700

    Vòng lặp FOR

---
+++ PHATKEO.cpp
@@ -0,0 +1,15 @@
+﻿/*
+Một giáo viên phát kẹo cho các em học sinh theo qui tắc sau: em đầu tiên được nhận 1 viên kẹo, em tiếp theo nhận nhiều hơn em trước đó 3 viên. 
+Hỏi nếu có N học sinh thì tổng số kẹo phải có để Giáo viên phát đủ cho cả lớp là bao nhiêu? (0 < N < 45000)
+*/
+#include<iostream>
+using namespace std;
+int main() {
+	unsigned long long n, sum = 0, temp = 1;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		sum += temp;
+		temp += 3;
+	}
+	cout << sum;
+}
